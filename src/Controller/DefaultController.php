a2acf6a8dcc74b1f2c97f8ad7ba72585e83c0223
Varkoff <virgileboris96@hotmail.fr>
2020-05-11 18:40:02 +0200

    Ajout du titre

---
+++ src/Controller/DefaultController.php
@@ -0,0 +1,21 @@
+<?php
+// src/Controller/WildController.php
+namespace App\Controller;
+
+use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+
+class DefaultController extends AbstractController
+{
+    /**
+     * @Route("/", name="app_index")
+     */
+    public function index(): Response
+    {
+        return $this->render(
+            '/index.html.twig', [
+            'welcome' => 'Wild Series',
+        ]);
+    }
+}
