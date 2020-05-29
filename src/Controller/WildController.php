0aac21d9ada03c5e027267dda99d15633f17eb0b
Varkoff <virgileboris96@hotmail.fr>
2020-05-29 12:50:49 +0200

    quest 5

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -1,6 +1,7 @@
 <?php
 // src/Controller/WildController.php
 namespace App\Controller;
+
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,10 +11,19 @@ class WildController extends AbstractController
     /**
      * @Route("/wild", name="wild_index")
      */
-    public function index() :Response
+    public function index(): Response
     {
-        return $this->render('wild/index.html.twig', [
+        return $this->render(
+            'wild/index.html.twig', [
             'website' => 'Wild Séries',
         ]);
     }
+//
+//    /**
+//     * @Route("/wild/show/{name}" name="wild_show")
+//     */
+//    public function show($slug): Response
+//    {
+//        return $this->render('wild/show.html.twig', ['seriename' => $slug]);
+//    }
 }
