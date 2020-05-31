d24c07f85fffdeac22b78a05e1c3be8fbe43c5da
Varkoff <virgileboris96@hotmail.fr>
2020-05-31 15:29:43 +0200

    quete 5

--- src/Controller/WildController.php
+++ src/Controller/WildController.php
@@ -1,6 +1,7 @@
 <?php
 // src/Controller/WildController.php
 namespace App\Controller;
+
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
@@ -10,10 +11,29 @@ class WildController extends AbstractController
     /**
      * @Route("/wild", name="wild_index")
      */
-    public function index() :Response
+    public function index(): Response
     {
-        return $this->render('wild/index.html.twig', [
+        return $this->render(
+            'wild/index.html.twig', [
             'website' => 'Wild Séries',
+        ]);
+    }
+
+    /**
+     * @Route("/wild/show/{slug<^[a-z-]+>}",  name="wild_show")
+     */
+    public function show($slug =""): Response
+    {
+
+        $slug = str_replace('-',' ', $slug);
+        $slug = ucwords($slug);
+        if (empty($slug)){
+            $slug = "Aucune série sélectionnée, veuillez choisir une série";
+        }
+
+        return $this->render(
+            'wild/show.html.twig', [
+            'slug' => $slug,
         ]);
     }
 }
